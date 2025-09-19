cf7270171eac4f186c86ec8a7ea823b656d418f2
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-09-19 19:36:06 -0400

    Fixed shotgun ui

--- Assets/Scripts/UI/ShotgunUI.cs
+++ Assets/Scripts/UI/ShotgunUI.cs
@@ -1,46 +1,22 @@
 using TMPro;
 using UnityEngine;
 
-public class ShotgunUI : UIPanel
+public class ShotgunUI : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private TextMeshProUGUI ammoText;
-
-    private Shotgun shotgun;
+    [SerializeField] private Shotgun shotgun;
 
     private void OnEnable()
     {
-        // Subscribe to UIManager events
-        UIManager.Instance.OnPanelChanged += HandleShowPanel;
-        UIManager.Instance.OnUIClose += HandleHideAllPanels;
-        StartCoroutine(SubscribeWhenReady());
+        shotgun.OnAmmoChanged += UpdateAmmoUI;
+        UpdateAmmoUI(shotgun.CurrentAmmo, shotgun.MaxAmmo);
     }
 
     private void OnDisable()
     {
         if (shotgun != null)
             shotgun.OnAmmoChanged -= UpdateAmmoUI;
-
-        if (UIManager.Instance != null)
-        {
-            UIManager.Instance.OnPanelChanged -= HandleShowPanel;
-            UIManager.Instance.OnUIClose -= HandleHideAllPanels;
-        }
-    }
-
-    private System.Collections.IEnumerator SubscribeWhenReady()
-    {
-        yield return new WaitForEndOfFrame();
-
-        shotgun = GetComponentInParent<Shotgun>();
-        if (shotgun == null)
-            shotgun = Object.FindFirstObjectByType<Shotgun>();
-
-        if (shotgun != null)
-        {
-            shotgun.OnAmmoChanged += UpdateAmmoUI;
-            UpdateAmmoUI(shotgun.CurrentAmmo, shotgun.MaxAmmo);
-        }
     }
 
     private void UpdateAmmoUI(int current, int max)
@@ -55,41 +31,5 @@ public class ShotgunUI : UIPanel
             if (ammoText != null)
                 ammoText.text = $"Ammo: {current} / {max}";
         }
-    }
-
-    /// <summary>
-    /// Hides the ammo text UI element.
-    /// </summary>
-    public void HideAmmoText()
-    {
-        if (ammoText != null)
-            ammoText.gameObject.SetActive(false);
-    }
-
-    /// <summary>
-    /// Shows the ammo text UI element.
-    /// </summary>
-    public void ShowAmmoText()
-    {
-        if (ammoText != null)
-            ammoText.gameObject.SetActive(true);
-    }
-
-    private void HandleShowPanel(UIPanel panel)
-    {
-        // Hide ammo text when any panel is shown
-        HideAmmoText();
-    }
-
-    private void HandleHideAllPanels()
-    {
-        // Show ammo text when all panels are hidden
-        ShowAmmoText();
-    }
-
-    // Implementation of abstract Init() from UIPanel
-    private protected override void Init()
-    {
-        
     }
 }
