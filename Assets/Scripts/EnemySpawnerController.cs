9dba918d7f35ec516ce4cf296dc7f2f77c3f65b2
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-09-18 22:39:12 -0400

    Enemies are now trigger to spawn when the player enters the trigger radius

--- Assets/Scripts/EnemySpawnerController.cs
+++ Assets/Scripts/EnemySpawnerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,13 +10,13 @@ public class EnemySpawnerController : MonoBehaviour
     [SerializeField] private LayerMask _playerLayer;
 
     [Header("Spawn Object Settings")]
-    [SerializeField] private Transform _spawner;
-    [SerializeField] private float _spawnerRange;
     [SerializeField] private GameObject _spawnObjectPrefab;
     private List<GameObject> listOfEnemies = new();
 
     private bool _hasPlayerEntered = false;
     private bool _hasEnemySpawned = false;
+
+    public event Action OnEnemyWaveComplete = delegate { };
 
     
 
@@ -44,28 +45,27 @@ public class EnemySpawnerController : MonoBehaviour
 
         while (num-- > 0)
         {
-            const float ABOVE_THE_FLOOR = 1f;
-
-            Vector3 spawnPos = _spawner.position;
-            spawnPos.x = Random.Range(-_spawnerRange - spawnPos.x, _spawnerRange + spawnPos.x);
-            spawnPos.z = Random.Range(-_spawnerRange + spawnPos.z, _spawnerRange + spawnPos.z);
-            spawnPos.y = ABOVE_THE_FLOOR;
-
-            GameObject enemyObj = Instantiate(_spawnObjectPrefab, spawnPos, Quaternion.identity);
+            GameObject enemyObj = Instantiate(_spawnObjectPrefab, GenerateSpawnCoordinates(), Quaternion.identity);
             listOfEnemies.Add(enemyObj);
         }
 
     }
 
     bool CheckIfPlayerEntered() => Physics.CheckSphere(transform.position, _triggerZoneRadius, _playerLayer);
+
+    Vector3 GenerateSpawnCoordinates()
+    {
+        const float ABOVE_THE_FLOOR = 1f;
+
+        Vector3 randomPos = UnityEngine.Random.insideUnitSphere * _triggerZoneRadius;
+        Vector3 spawnPoint = new Vector3(transform.position.x + randomPos.x, ABOVE_THE_FLOOR, transform.position.z + randomPos.z);
+        return spawnPoint;
+    }
     
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(_triggerZone.position, _triggerZoneRadius);
-
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawSphere(_spawner.position, 1f);
     }
 }
