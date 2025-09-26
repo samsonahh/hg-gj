28e9121ca799a4be8a68a3b349dc0c3bded23a53
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2025-09-25 20:49:43 -0400

    Spawner function done. Working on getting enemies to spawn outside of walls

--- Assets/Scripts/EnemySpawnerController.cs
+++ Assets/Scripts/EnemySpawnerController.cs
@@ -1,22 +1,26 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawnerController : MonoBehaviour
 {
     [Header("Trigger Zone Settings")]
     [SerializeField] private Transform _triggerZone;
-    [SerializeField] private float _triggerZoneRadius;
+    [SerializeField] private float _triggerZoneSize;
     [SerializeField] private LayerMask _playerLayer;
 
     [Header("Spawn Object Settings")]
     [SerializeField] private GameObject _spawnObjectPrefab;
-    private List<GameObject> listOfEnemies = new();
+    [SerializeField] private float _spawnDelay;
 
+    [Header("Game Settings")]
+    [SerializeField] private float _maxEnemiesOnFieldAtATime;
+    [SerializeField] private float _totalNumberOfEnemies;
+    [SerializeField] private float _killCounter = 0;
+    private float _currentEnemiesOnField = 0;
+    private float _intitalSpawnCounter = 0;
     private bool _hasPlayerEntered = false;
-    private bool _hasEnemySpawned = false;
-
-    public event Action OnEnemyWaveComplete = delegate { };
 
     
 
@@ -32,32 +36,80 @@ public class EnemySpawnerController : MonoBehaviour
     {
         if(CheckIfPlayerEntered() && !_hasPlayerEntered)
         {
-            IntialSpawnEnemy();
+            IntialSpawnEnemies();
             _hasPlayerEntered = true;
         }
+
+        if (_currentEnemiesOnField >= _maxEnemiesOnFieldAtATime) CancelInvoke("SpawnEnemy");
+        if (_killCounter >= _totalNumberOfEnemies)
+        {
+            CancelInvoke("SpawnEnemy");
+            Debug.LogWarning("You Win");
+        }
+        
     }
 
-    void IntialSpawnEnemy()
+    public void HandleDeathLogic()
     {
-        _hasEnemySpawned = true;
+        _killCounter++;
+        _currentEnemiesOnField--;
 
-        int num = 5;
+        if (_killCounter < _totalNumberOfEnemies) Invoke("SpawnEnemy", _spawnDelay); 
+    }
 
-        while (num-- > 0)
+    void IntialSpawnEnemies()
+    {
+        float INTIAL_SPAWN_TIME = 0f;
+        InvokeRepeating("SpawnEnemy", INTIAL_SPAWN_TIME, _spawnDelay);
+    }
+
+    void SpawnEnemy()
+    {
+        if (_intitalSpawnCounter != _maxEnemiesOnFieldAtATime) _intitalSpawnCounter++;
+
+        Vector3 spawnCoordinates = GenerateSpawnCoordinates();
+
+        Ray ray = new Ray(spawnCoordinates, Vector3.down);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
         {
-            GameObject enemyObj = Instantiate(_spawnObjectPrefab, GenerateSpawnCoordinates(), Quaternion.identity);
-            listOfEnemies.Add(enemyObj);
+            //if (hit.collider != null)
+            //{
+            //    Debug.LogWarning(hit.collider.gameObject.name);
+            //}
+            NavMeshHit navHit;
+
+            // 2 is suppose to what I assume is the walkable area index
+            if (NavMesh.SamplePosition(hit.transform.position, out navHit, 1.0f, 1))
+            {
+                Debug.LogWarning("Valid Spawn");
+                
+            }
+            else
+            {
+                Debug.LogWarning("Invalid Spawn");
+            }
         }
 
+
+        GameObject enemyObj = Instantiate(_spawnObjectPrefab, spawnCoordinates, Quaternion.identity);
+        enemyObj.GetComponent<HealthEntity>().OnDied += HandleDeathLogic;
+        _currentEnemiesOnField++;
     }
 
-    bool CheckIfPlayerEntered() => Physics.CheckSphere(transform.position, _triggerZoneRadius, _playerLayer);
+    void IsSpawnValid()
+    {
+        Vector3 spawnCoordinates = GenerateSpawnCoordinates();
+    }
+
+    bool CheckIfPlayerEntered() => Physics.CheckSphere(transform.position, _triggerZoneSize, _playerLayer);
 
     Vector3 GenerateSpawnCoordinates()
     {
         const float ABOVE_THE_FLOOR = 1f;
 
-        Vector3 randomPos = UnityEngine.Random.insideUnitSphere * _triggerZoneRadius;
+        Vector3 randomPos = Random.insideUnitSphere * _triggerZoneSize;
         Vector3 spawnPoint = new Vector3(transform.position.x + randomPos.x, ABOVE_THE_FLOOR, transform.position.z + randomPos.z);
         return spawnPoint;
     }
@@ -66,6 +118,6 @@ public class EnemySpawnerController : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_triggerZone.position, _triggerZoneRadius);
+        Gizmos.DrawWireSphere(_triggerZone.position, _triggerZoneSize);
     }
 }
